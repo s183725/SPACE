b89579336a8e0e49bf5bf3017bb1766bc0236be5
s183725 <51827251+s183725@users.noreply.github.com>
2019-06-19 15:25:17 +0200

    Add files via upload

--- c files/directional.c
+++ c files/directional.c
@@ -1,11 +1,18 @@
-#include "shipcontrol.h"
+#include "inputRead.h"
+#include "stdbool.h"
 #include "vectors.h"
 
-uint8_t turnVAL(){
-    uint8_t ch, tVAL;
-    ch = wasdIN();  //læser input fra A og D
+int32_t initial = 1;
 
-    switch(ch) //at dreje moduret svare til at plusse med 1
+int32_t turnVAL(){
+    uint8_t ch;
+    int16_t tVAL;
+    //ch = uart_get_char();  // skal læser input fra A og D
+
+    ch = 97;
+    char character = ch;
+
+    switch(ch) //at dreje moduret svarer til 1
     {
     case 97:
         return tVAL = 1;
@@ -16,12 +23,147 @@ uint8_t turnVAL(){
     }
 }
 
-void directionVAL(){
-    uint8_t dirVAL;
-    dirVAL = dirVAL + turnVAL();
+void rotVAL(int32_t turnVAL){
+    uint32_t rVAL, initVAL;
+    //uint8_t *dirArray[16] = {(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 )};
+    //if(turnVAL() == )
+    if (initVAL != 0){
+        initVAL = 0;
+        rVAL = 0;
+    } else {
+        initVAL = 1;
+    }
+
+    if (turnVAL == 1) {
+        //rVAL = (45 >> 1);
+    } else if (turnVAL == -1) {
+        //rVAL = 0 - (45 >> 1);
+    } else if (turnVAL == 0) {
+        rVAL == 0;
+    }
+    return rVAL;
+}
+
+int32_t dirVAL(int32_t turnVAL){
+    int32_t dVAL;
+    int8_t active;
+
+    if (initial > 0) {
+        initial = 0;
+        dVAL = 0;
+    }
+
+        if (turnVAL == 1) {
+            dVAL += 1;
+        } else if (turnVAL == -1) {
+            dVAL -= 1;
+        } else if (turnVAL == 0) {
+            dVAL = 0;
+        }
+
+        if(dVAL > 15){ // Accounts for above radians
+            dVAL -= 15;
+        } else if (dVAL < 0) {
+            dVAL += 15;
+        }
+
+    return dVAL;
+}
+
+void dmem(int32_t a){
+    int8_t dVAL2;
+}
 
 
-    //switch()
+void directionArray(int32_t turnVAL){
+    VECTOR dirV[16] = {
+        {3,0},
+        {2,1},
+        {1,1},
+        {1,2},
+        {0,3},
+        {-1,2},
+        {-1,1},
+        {-2,1},
+        {-3,0},
+        {-2,-1},
+        {-1,-1},
+        {-1,-2},
+        {0,-3},
+        {1,-2},
+        {1,-1},
+        {2,-1},
+    };
+
+    uint8_t *dArray_p;
+
+    if(turnVAL == 1){
+        dArray_p++;
+    } else if (turnVAL == (-1)){
+        dArray_p--;
+    }
+
+
+
+    return (*dArray_p);
+}
+
+
+void directionVector(int32_t dVAL, VECTOR *v){
+
+    switch(dVAL){
+    case 0:
+        (*v).x = 3;
+        (*v).y = 0;
+    case 1:
+        (*v).x = 2;
+        (*v).y = 1;
+    case 2:
+        (*v).x = 1;
+        (*v).y = 1;
+    case 3:
+        (*v).x = 1;
+        (*v).y = 2;
+    case 4:
+        (*v).x = 0;
+        (*v).y = 3;
+    case 5:
+        (*v).x = -1;
+        (*v).y = 2;
+    case 6:
+        (*v).x = -1;
+        (*v).y = 1;
+    case 7:
+        (*v).x = -2;
+        (*v).y = 1;
+    case 8:
+        (*v).x = -3;
+        (*v).y = 0;
+    case 9:
+        (*v).x = -2;
+        (*v).y = -1;
+    case 10:
+        (*v).x = -1;
+        (*v).y = -1;
+    case 11:
+        (*v).x = -1;
+        (*v).y = -2;
+    case 12:
+        (*v).x = 0;
+        (*v).y = -3;
+    case 13:
+        (*v).x = 1;
+        (*v).y = -2;
+    case 14:
+        (*v).x = 1;
+        (*v).y = -1;
+    case 15:
+        (*v).x = 2;
+        (*v).y = -1;
+    }
+}
+
+void rawDirection(){
 
 }
 
--- c files/inputRead.c
+++ c files/inputRead.c
@@ -60,4 +60,38 @@ uint8_t spaceIN(){
     }
 }
 
+char directCTRL(void) {
 
+    char inKey;
+
+    switch (wasdIN())
+    {
+    case 119: // w forward
+        return inKey = 'w';
+    case 115: // s intended dampener
+        return inKey = 's';
+    case 97: // a for anti-clockwise rotation
+        return inKey = 'a';
+    case 100: // d for clockwise rotation
+        return inKey = 'd';
+    }
+
+    switch(numIN())
+    {
+    case 49: // char 1
+        return inKey = '1';
+    case 50: // char 2
+        return inKey = '2';
+    case 51: // char 3
+        return inKey = '3';
+    case 52: // char 4
+        return inKey = '4';
+    }
+
+    switch(rawIN())
+    {
+    case 32:
+        return inKey = ' ';
+    }
+
+}
